2100c012f5622e0279799c3a80bc0bf4e78b06f6
TimoMet <timetais22@gmail.com>
2023-03-01 12:59:45 +0100

    recycler view movement

--- app/src/main/java/com/example/todofast/MainActivity.kt
+++ app/src/main/java/com/example/todofast/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.todofast
 import TaskAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var taskAdapter: TaskAdapter = TaskAdapter(arrayListOf(), this)
+    private var listView: RecyclerView? = null
+    var values = arrayListOf<Task>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val values = arrayListOf(
+         values = arrayListOf(
             Task("tafast"),
             Task("tonperefast"),
             Task("todofast"),
@@ -21,12 +27,54 @@ class MainActivity : AppCompatActivity() {
             Task("the fast"),
             Task("not so fast"),
         )
+        taskAdapter = TaskAdapter(values, this)
+        listView = findViewById(R.id.list)
+        listView!!.adapter = taskAdapter
+        listView!!.layoutManager = LinearLayoutManager(this)
 
-        val adapter = TaskAdapter(values, this)
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.Callback() {
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                // set movement left and right forbidden
+                val swipeFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                return makeMovementFlags(ItemTouchHelper.ACTION_STATE_IDLE, swipeFlags)
+            }
 
-        val listView = findViewById<RecyclerView>(R.id.list)
-        listView.adapter = adapter
-        listView.layoutManager = LinearLayoutManager(this)
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // Called when an item is dragged and dropped to a new position
+                val fromPosition = viewHolder.adapterPosition
+                val toPosition = target.adapterPosition
+                // Update your data set with the new item order
+                Collections.swap(values, fromPosition, toPosition)
+                taskAdapter.notifyItemMoved(fromPosition, toPosition)
+
+                println("MOVED")
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // Called when an item is swiped to the left or right
+                val position = viewHolder.adapterPosition
+                // Remove the item from your data set and notify the adapter
+                values.removeAt(position)
+                taskAdapter.notifyItemRemoved(position)
+
+                println("REMOVED")
+            }
+        })
+        itemTouchHelper.attachToRecyclerView(listView)
+
+    }
+
+    fun setItemToFirst(index: Int) {
+        Collections.swap(values, index, 0)
+        taskAdapter.notifyItemMoved(index, 0)
     }
 
 }
--- app/src/main/java/com/example/todofast/TaskAdapter.kt
+++ app/src/main/java/com/example/todofast/TaskAdapter.kt
@@ -1,22 +1,41 @@
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todofast.MainActivity
 import com.example.todofast.R
 import com.example.todofast.Task
 
 class TaskAdapter(private val items: ArrayList<Task>, private val context: Context) :
-    RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
+    RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view =
-            LayoutInflater.from(context).inflate(R.layout.task, parent, false)
-        return ViewHolder(view)
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        var titleTextView: TextView = itemView.findViewById(R.id.title)
+        init {
+            itemView.findViewById<ImageButton>(R.id.actionTask).setOnClickListener(this)
+        }
+        override fun onClick(view: View?) {
+            // Get the position of the item that was clicked
+            val position: Int = adapterPosition
+            // Do something with the clicked item
+            Log.d("TaskAdapter", "Item clicked at position $position")
+
+            val mainActivity = view?.context as MainActivity
+            mainActivity.setItemToFirst(position)
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.task, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val task = items[position]
         holder.titleTextView.text = task.title
     }
@@ -25,7 +44,4 @@ class TaskAdapter(private val items: ArrayList<Task>, private val context: Conte
         return items.size
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var titleTextView: TextView = itemView.findViewById(R.id.title)
-    }
 }
